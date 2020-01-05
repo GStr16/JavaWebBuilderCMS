a2c64dd5cd0fb31cf6aa745844dc6b81befd6a9a
GStr16 <stroeandrei483@gmail.com>
2020-01-05 16:06:15 -0500

    I need to fix decryption login urgently!

--- src/loginSystem/Controller.java
+++ src/loginSystem/Controller.java
@@ -15,7 +15,12 @@ import java.sql.ResultSet;
 
 public class Controller {
 
+    Mail checkerEmail = new Mail();
+    AES aes = new AES();
+
+    String secretKey = "JavaWebBuilderCMS";
     boolean isOkay = false;
+
     // Login form
     @FXML
     private TextField userField;
@@ -38,19 +43,36 @@ public class Controller {
     static Connection con = null;
     static PreparedStatement pst = null;
     public void hello(ActionEvent env) {
+
         String sql = "SELECT * FROM root where (USERNAME=? and PASSWORD=?) OR (EMAIL=? and PASSWORD=?)";
+
+
         try {
+
+            String pass = aes.encrypt(passField.getText(), secretKey);
+            String email = aes.encrypt(emailField.getText(), secretKey);
+            String user = aes.encrypt(usernameField.getText(), secretKey);
+
+            System.out.println(aes.decrypt(pass, secretKey));
+            System.out.println(aes.decrypt(email, secretKey));
+            System.out.println(aes.decrypt(user, secretKey));
+
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://hosting1993073.online.pro:3306/00286862_test", "00286862_test", "Y6ufde_e");            System.out.println("Working!");
             pst = con.prepareStatement(sql);
-            pst.setString(1,userField.getText());
-            pst.setString(2,passwordField.getText());
-            pst.setString(3, userField.getText());
-            pst.setString(4, passwordField.getText());
+            pst.setString(1,user);
+            pst.setString(2,pass);
+            pst.setString(3, email);
+            pst.setString(4, pass);
             ResultSet rs = pst.executeQuery();
             if(rs.next()) {
                 System.out.println("Working account");
             }
+
+
+            System.out.println(aes.decrypt(pass, secretKey));
+            System.out.println(aes.decrypt(email, secretKey));
+            System.out.println(aes.decrypt(user, secretKey));
         }
         catch (Exception ex) {
             System.out.println("Error " +ex.getMessage());
@@ -58,7 +80,7 @@ public class Controller {
     }
     @FXML
     public void registerScene(ActionEvent env) throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("Register.fxml"));
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("register.fxml"));
         rootPanelLogin.getChildren().setAll(pane);
     }
     @FXML
@@ -73,31 +95,44 @@ public class Controller {
     }
 
     public void Register(ActionEvent env) {
+
         isOkay = false;
         String sql = "INSERT INTO root(USERNAME, EMAIL, PASSWORD) values (?,?,?)";
         try {
 
-            if(!passField.equals("") && !usernameField.equals("") && !emailField.equals(""))
+            if( passField.getText().equals("") && usernameField.getText().equals("") && emailField.getText().equals(""))
+                System.out.println("Error "+isOkay);
+            else {
                 isOkay = true;
-            else
-                System.out.println("Pleasw try again");
+                System.out.println(isOkay);
+            }
 
-
-            if (isOkay = true) {
+            if (isOkay == true) {
                 Class.forName("com.mysql.jdbc.Driver");
                 con = DriverManager.getConnection("jdbc:mysql://hosting1993073.online.pro:3306/00286862_test", "00286862_test", "Y6ufde_e");
                 System.out.println("Working!");
                 System.out.println("Working!");
                 pst = con.prepareStatement(sql);
 
-                pst.setString(1, usernameField.getText());
-                pst.setString(2, emailField.getText());
-                pst.setString(3, passField.getText());
+                pst.setString(1, aes.encrypt(usernameField.getText(), secretKey));
+                pst.setString(2, aes.encrypt(emailField.getText(), secretKey) );
+                pst.setString(3, aes.encrypt(passField.getText(), secretKey) );
+
+                System.out.println(aes.encrypt(usernameField.getText(), secretKey));
+                System.out.println(aes.encrypt(emailField.getText(), secretKey) );
+                System.out.println((aes.encrypt(passField.getText(), secretKey) ));
+                System.out.println("Decription:");
+                String pass = aes.encrypt(passField.getText(), secretKey);
+                String email = aes.encrypt(emailField.getText(), secretKey);
+                String user = aes.encrypt(usernameField.getText(), secretKey);
+                System.out.println(aes.decrypt(pass, secretKey));
+                System.out.println(aes.decrypt(email, secretKey));
+                System.out.println(aes.decrypt(user, secretKey));
 
                 System.out.println("Working");
 
                 pst.executeUpdate();
-                JOptionPane.showMessageDialog(null, "Succesfull!");
+//                JOptionPane.showMessageDialog(null, "Succesfull!");
 
             }
         }
